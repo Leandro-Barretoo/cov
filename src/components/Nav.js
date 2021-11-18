bd22164e6d5384fe66de4393acbb566106db7a94
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-18 08:53:30 -0800

    Add navigaion styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,19 @@
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import logo from '../assets/Covid.png';
 import './Nav.css';
 
-const Nav = (props) => {
-  const { display } = props;
-  return (
-    <>
-      <div className="Navigation">
-        <span className={display}><Link to="/" className="Link">&lt; Back</Link></span>
-        <span className="Logo">CoronaWide</span>
-      </div>
-      <div className="Slogan-Cont">
-        <img className="Logo" src={logo} alt="covid logo" />
-        <span className="Slogan">
-          World&apos;s CoronaVirus Statistics
-        </span>
-      </div>
-    </>
-  );
-};
-
-Nav.defaultProps = {
-  display: '',
-};
-
-Nav.propTypes = {
-  display: PropTypes.string,
-};
+const Nav = () => (
+  <>
+    <div className="Navigation">
+      <span className="Logo"><Link to="/" className="Link">CoronaWide</Link></span>
+    </div>
+    <div className="Slogan-Cont">
+      <img className="Logo" src={logo} alt="covid logo" />
+      <span className="Slogan">
+        World&apos;s CoronaVirus Statistics
+      </span>
+    </div>
+  </>
+);
 
 export default Nav;
